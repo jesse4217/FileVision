b8feca1088512ab202cb47f16cf3b08445640031
Jesse <jessejiang010@gmail.com>
2024-08-31 15:43:38 +0900

    Added Package

--- Sources/FileVision/FileVision.swift
+++ Sources/FileVision/FileVision.swift
@@ -1,2 +1,11 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import Foundation
+
+public struct SDK {
+    public static func doSomething() {
+        print("Do Something!")
+    }
+}
+
